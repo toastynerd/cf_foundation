606d3a21852482db872d0960fce8c4cfc2d1ef83
tylermorgan86 <tylermorgan86@gmail.com>
2013-05-11 16:35:30 -0700

    Initial foundation project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foundation::Application.config.secret_token = '2b8a4bf83a6afcc2aedac995dd559712ae0049e076d23b9ce2ae0fc110d09a4e03884232815d27f4238d3dfd934cc8676885d1188a8663e2b53ddcd023fc2f5d'
